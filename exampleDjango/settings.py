9232480cfbe95d293e4e71b1a2c16d3912fe97a7
Esa Obande <stash.circuit@gmail.com>
2021-04-26 18:32:26 +0100

    15th try

--- exampleDjango/settings.py
+++ exampleDjango/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-oyogqnz9&=%#_z#ld@g%vf7956bi!60f_b*t&rjmmejfe^&%qf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['esasdjangoblog.herokuapp.com']
 
 
 # Application definition
